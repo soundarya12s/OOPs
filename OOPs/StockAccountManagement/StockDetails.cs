33c2ec54e3f64227360ceb758924828b6c0b6406
S Soundarya <soundarya.s@tvsnext.com>
2023-07-20 13:19:43 +0530

    [Soundarya]Add: Added inventory management.

---
+++ OOPs/StockAccountManagement/StockDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPs.StockAccountManagement
+{
+    public class StockDetails
+    {
+
+        public string StockName { get; set; }
+        public int NoOfShares { get; set; }
+        public int SharePrice { get; set; }
+
+    }
+}
